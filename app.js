fdeeeefbcec2422dcabaf37e77a681bc15462033
wreckster1507 <sarthakaitha1507@gmail.com>
2024-01-01 14:28:39 +0530

    app.js changes

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
-//process.env.DB_URL || 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+const dbUrl = process.env.DB_URL ;
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
